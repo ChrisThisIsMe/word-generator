e4f20a5de562105701e84cb7879b28194a46c380
ChrisThisIsMe <christophe.siegenthaler@gmail.com>
2025-04-18 15:33:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ st.set_page_config(page_title="Outils de génération", page_icon="⚙️")
 
 st.title("Bienvenue sur ton générateur !")
 
-menu = st.selectbox("Choisis un outil 👇", [
-    "🏷️ Générateur Markdown (.md)",
-    "📝 Générateur Word Stylé (.docx)"
-])
+menu = st.selectbox(
+    "Choisis un outil 👇",
+    [
+        "--- Choisis un outil ---",
+        "🏷️ Générateur Markdown (.md)",
+        "📝 Générateur Word Stylé (.docx)"
+    ]
+)
 
 if menu == "🏷️ Générateur Markdown (.md)":
-    st.header("Générateur Markdown")
-    st.markdown("👉 [Clique ici pour lancer le générateur Markdown](https://md-generator.streamlit.app)")
+    st.markdown("""
+        <meta http-equiv="refresh" content="0; url='https://md-generator.streamlit.app'" />
+    """, unsafe_allow_html=True)
 
 elif menu == "📝 Générateur Word Stylé (.docx)":
-    st.header("Générateur Word Stylé")
-    st.markdown("👉 [Clique ici pour lancer le Generator Word Style](https://word-generator-style.streamlit.app)")
+    st.markdown("""
+        <meta http-equiv="refresh" content="0; url='https://word-generator-style.streamlit.app'" />
+    """, unsafe_allow_html=True)
